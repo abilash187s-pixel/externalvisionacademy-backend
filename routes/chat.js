703e6081961bc3e9cdb2591ab32a7bac21d277e8
ABILASH <abilash187s@gmail.com>
2025-12-31 11:48:54 +0530

    test

--- routes/chat.js
+++ routes/chat.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { chatWithAI } from "../controllers/chat.controller";
+import { chatWithAI } from "../controllers/chat.controller.js";
 
 const router = express.Router();
 
